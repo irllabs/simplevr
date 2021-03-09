983188fc88b0fa717e8f8fd99c9edf55d308af04
Lazar Mitic <lazar.mitic@outlook.com>
2021-03-09 21:31:23 +0100

    Add user and public story sections (#45)

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
+
 import user from './user';
 import display from './display';
-
+import userStories from './user-stories';
+import publicStories from './public-stories';
 
 export default combineReducers({
     user,
-    display
+    display,
+    userStories,
+    publicStories,
 });
